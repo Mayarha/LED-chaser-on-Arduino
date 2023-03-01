7abff9d13166d30d0f639d17f8b489e3e7facb37
Mzameli Mashiyi <84459673+Mayarha@users.noreply.github.com>
2023-03-01 11:06:36 +0200

    Add files via upload
    You will need an arduino and about 7 LED's to do this project. I am uploading the code.

---
+++ Chaser.ino
@@ -0,0 +1,38 @@
+bool userIn;
+int delayTime = 0;
+
+void setup() {
+  for(int pinNum = 2; pinNum < 12; pinNum++){
+  pinMode(pinNum, OUTPUT);
+  }
+}
+
+void loop(){
+  //switch forward
+  for(int pinNum = 2; pinNum < 12; pinNum++){
+  digitalWrite(pinNum, HIGH);  //switch on
+  hDelay(100);  //delay
+  digitalWrite(pinNum, LOW); //switch off
+  }
+  
+  //switch backwards
+  for(int pinNum = 11; pinNum > 1; pinNum--){
+  digitalWrite(pinNum, HIGH);
+  hDelay(100);
+  digitalWrite(pinNum, LOW);
+  }
+}
+  /////
+  void hDelay(int input){
+  userIn = digitalRead(12);
+    if(userIn == true){
+    delayTime = 0;
+    }  
+    
+    if (delayTime > 50 ){
+      while(true){}
+    }
+  
+  delay(input);
+}
+
